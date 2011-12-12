f324968a3625ad4c1a1167af32bf2eb66fbf0020
Douglas Miller <doug@c4tech.com>
2011-12-12 09:51:13 -0600

    testing

--- sites/all/themes/loo/templates/node--film_clip.tpl.php
+++ sites/all/themes/loo/templates/node--film_clip.tpl.php
@@ -103,7 +103,7 @@
 	      
         drupal_add_js('sites/all/libraries/popcorn-js/popcorn.js');
         drupal_add_js('sites/all/libraries/popcorn-js/plugins/drupal/popcorn.drupal.js');
-        drupal_add_js('sites/all/modules/popcorn/js/controls.js');
+        //drupal_add_js('sites/all/modules/popcorn/js/controls.js');
         //drupal_add_js(path_to_theme() . '/js/video-controls.js');
 	      drupal_add_css(path_to_theme() . '/css/video-controls.css');
         drupal_add_css('modules/contextual/contextual.css');
