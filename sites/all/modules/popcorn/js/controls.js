2dabff7c303cab4a50dc4182eb20744276c2679f
Douglas Miller <doug@c4tech.com>
2012-01-15 16:08:40 -0600

    ie debugging

--- sites/all/modules/popcorn/js/controls.js
+++ sites/all/modules/popcorn/js/controls.js
@@ -50,7 +50,6 @@ function Controller(){
 Controller.prototype.catchHistory = function(index){
 	
 	var vidData = this.history.loadHistory(index);
-	console.log(vidData);
 
 	//remove existing Track Events
 	var kernels = popcorn.getTrackEvents();
@@ -173,6 +172,7 @@ function HistoryManager(controller){
 }
 
 HistoryManager.prototype.loadHistory = function(i){
+	console.log(this.historyList);
 	var selected = this.historyList[i];
 	this.historyList = this.historyList.slice(0, i);
 	return selected;
